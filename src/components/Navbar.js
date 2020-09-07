11d02702c77cde3d8277f8b384975e4ab222a8c7
starskynder <24276076+starskynder@users.noreply.github.com>
2020-09-07 13:34:36 +0200

    Refactor component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,74 +2,45 @@ import React, { useState, useEffect } from "react";
 
 const Navbar = ({ sections, logo, login }) => {
   const [openMenu, setOpenMenu] = useState(false);
-  const [menu, setMenu] = useState(true);
+
   const items = [...sections];
   console.log(items);
 
-  /*   useEffect(() => {
-    const verify = window.matchMedia("(min-width: 400px)");
-
-    window.addEventListener("resize", (e) => {
-      if (e.matches) {
-        setMenu(false);
-        setOpenMenu(true);
-      } else {
-        setOpenMenu(false);
-        setMenu(true);
-      }
-    });
-  }); */
-  const [width, setWidth] = useState();
-  useEffect(() => {
-    setWidth(window.innerWidth);
-    window.addEventListener("resize", () => setWidth(window.innerWidth));
-  }, []);
-
-  useEffect(() => {
-    if (width > 1200) {
-      setMenu(false);
-      setOpenMenu(true);
-    } else {
-      setMenu(true);
-    }
-  }, [width]);
-
-  console.log(menu);
   return (
     <nav className="nav">
       <div className="nav__navigation">
         <img src={logo} alt="" className="nav__logo" />
-        {menu ? (
-          <button className="nav__btn" onClick={() => setOpenMenu(!openMenu)}>
-            <span className="nav__btn--line"></span>
-            <span className="nav__btn--line"></span>
-            <span className="nav__btn--line"></span>
-          </button>
-        ) : null}
+
+        <button className="nav__btn" onClick={() => setOpenMenu(!openMenu)}>
+          <span className="nav__btn--line"></span>
+          <span className="nav__btn--line"></span>
+          <span className="nav__btn--line"></span>
+        </button>
       </div>
-      {openMenu ? (
-        <>
-          <ul className="nav__list">
-            {items.map((item, index) => {
-              return (
-                <li className="nav__item" key={index}>
-                  <a href={"#" + item} className="nav__link">
-                    {item}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-          <div className="nav__login">
-            {login ? (
-              <>
-                <button className="btn__login"> login</button>
-                <button className="btn__signup">sign up</button>
-              </>
-            ) : null}
-          </div>
-        </>
-      ) : null}
+
+      <>
+        <ul className={openMenu ? "nav__list " : "nav__list nav__close"}>
+          {items.map((item, index) => {
+            return (
+              <li className="nav__item" key={index}>
+                <a
+                  href={"#" + item}
+                  className="nav__link"
+                  onClick={() => setOpenMenu(false)}
+                >
+                  {item}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className={openMenu ? "nav__login" : "nav__login nav__close"}>
+          <>
+            <button className="btn__login"> login</button>
+            <button className="btn__signup">sign up</button>
+          </>
+        </div>
+      </>
     </nav>
   );
 };
