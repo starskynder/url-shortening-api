52551b044e7f863bac610c817b1a3862941905fb
starskynder <24276076+starskynder@users.noreply.github.com>
2021-01-18 15:34:26 +0100

    Create build

---
+++ build/precache-manifest.a8e94c5fd4541e46e7c10eb15a7a58dc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb39d4d7c4abaa0da1c2289b4e0d5f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9023b96b61938a2073f",
+    "url": "/static/css/main.cb244f09.chunk.css"
+  },
+  {
+    "revision": "f1b20a57cc038c46c137",
+    "url": "/static/js/2.4313ec01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4313ec01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9023b96b61938a2073f",
+    "url": "/static/js/main.eb51a786.chunk.js"
+  },
+  {
+    "revision": "5a54e6fa03313f2718db",
+    "url": "/static/js/runtime-main.e500c84c.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/static/media/logo.6387840f.svg"
+  }
+]);
