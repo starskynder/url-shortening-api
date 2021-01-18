43fd55e4f01d9d6e35988afd9e92ddbc275b18fc
starskynder <24276076+starskynder@users.noreply.github.com>
2021-01-18 16:43:01 +0100

    Re-run build

---
+++ build/precache-manifest.8037046da0475b14531a91fd0d54b5f5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed398aa438773ac103ab321fdb96cd1a",
+    "url": "/url-shortening-api/index.html"
+  },
+  {
+    "revision": "d90a33034dbbaf42a484",
+    "url": "/url-shortening-api/static/css/main.817de4d5.chunk.css"
+  },
+  {
+    "revision": "f1b20a57cc038c46c137",
+    "url": "/url-shortening-api/static/js/2.4313ec01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/url-shortening-api/static/js/2.4313ec01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90a33034dbbaf42a484",
+    "url": "/url-shortening-api/static/js/main.3340a21c.chunk.js"
+  },
+  {
+    "revision": "d4809ef8774cb5ed381a",
+    "url": "/url-shortening-api/static/js/runtime-main.49323ceb.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/url-shortening-api/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/url-shortening-api/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/url-shortening-api/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/url-shortening-api/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/url-shortening-api/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/url-shortening-api/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/url-shortening-api/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/url-shortening-api/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/url-shortening-api/static/media/logo.6387840f.svg"
+  }
+]);
